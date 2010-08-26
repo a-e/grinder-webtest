087e258cd4abccc82e6b7dd89e7912687c314a78
Eric Pierce <wapcaplet88@gmail.com>
2010-08-26 11:40:28 -0600

    Minor doc configuration for title and owner

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Grinder Webtest documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -175,7 +175,7 @@ htmlhelp_basename = 'GrinderWebtestdoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'GrinderWebtest.tex', u'Grinder Webtest Documentation',
-   u'Eric Pierce', 'manual'),
+   u'Automation Excellence', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
