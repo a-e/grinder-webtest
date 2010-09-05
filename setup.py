dade2808c4d81c4c5cc14e5f21fd3507bb038e36
Eric Pierce <wapcaplet88@gmail.com>
2010-09-05 14:43:27 -0600

    Added MANIFEST.in and setup.py to prepare for release

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# setup.py
+
+"""Installer for Grinder Webtest.
+"""
+
+from distutils.core import setup
+
+setup(name='Grinder Webtest',
+      version='0.1',
+      description='Wrapper for running .webtest files with Grinder',
+      author='Eric Pierce',
+      author_email='wapcaplet88@gmail.com',
+      url='http://www.automation-excellence.com/software/grinder-webtest',
+      license='Simplified BSD License',
+      packages=['webtest'],
+     )
+
+
