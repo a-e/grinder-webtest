a1268043f41fb5c841b19dc5a91f5a777050059b
Eric Pierce <wapcaplet88@gmail.com>
2011-10-03 12:57:34 -0600

    Remote setup/teardown (not needed with pytest)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,25 +1,10 @@
 # __init__.py
 
-"""Setup and teardown methods for Grinder Webtest unit tests.
+"""Global test initialization
 """
 
 import os
-import sys
 
 # Directory where test data files are stored
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
-
-# Package-level setup and teardown
-
-def setup():
-    """Package-level setup.
-    """
-    # In case it's not installed systemwide
-    sys.path.append(os.path.abspath('..'))
-
-
-def teardown():
-    """Package-level teardown.
-    """
-    pass
 
